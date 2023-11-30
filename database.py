85ef6d291bafcce09ce8349a02b897da2dc465cc
rasadov <raufasadov23@gmail.com>
2023-11-30 16:51:02 +0400

    added register, sell functions

--- database.py
+++ database.py
@@ -6,12 +6,14 @@ users = pd.read_csv("users.csv")
 database = pd.read_csv("database.csv")
 
 
-def add(producer, model, type, year, hp, cap, color):
+def add(producer, model, type, year, hp, cap, color, user):
     with open('database.csv', 'a', newline='') as storage2:
         csv_writer = csv.writer(storage2, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-        csv_writer.writerow((producer, model, type, year, hp, cap, color))
+        csv_writer.writerow(
+            (producer, model, type, year, hp, cap, color, user))
+    database = pd.read_csv("database.csv")
+    print("Success")
 
 
 def register(username, name: None, surname: None, birthday: None, password):
@@ -21,23 +23,25 @@ def register(username, name: None, surname: None, birthday: None, password):
     if len(password) < 8:
         print("password is too short.")
         return 0
-    with open('database.csv', 'a', newline='') as storage2:
+    with open('users.csv', 'a', newline='') as storage2:
         csv_writer = csv.writer(storage2, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        csv_writer.writerow(username, name, surname, birthday, password)
+        csv_writer.writerow((username, name, surname, birthday, password))
 
 
 def login(username, password):
     if len(users.loc[users["username"] == username]) == 1:
         if len(users.loc[(users["username"] == username) & (users["password"] == password)]) == 1:
-            return username
+            return 1
     else:
+        username = 0
         print("No such account")
     return 0
 
 
 def dashboard(username):
+    database = pd.read_csv("database.csv")
     user = users.loc[users['username'] == username]
     print(f"Good day {user['name'][0]} {user['surname'][0]}")
     print("Your listings:")
@@ -45,6 +49,23 @@ def dashboard(username):
           ["producer", "model", "type", "year", "hp", "cap", "color"]])
 
 
+def username_exist(username):
+    if len(users.loc[users["username"] == username]) == 1:
+        return False
+    return True
+
+
+def show_cars():
+    database = pd.read_csv("database.csv")
+    print(database)
+
+
+def show_car(car):
+    users = pd.read_csv("users.csv")
+    database = pd.read_csv("database.csv")
+    owner = database.loc[database["producer"] == car]["owner"]
+    print(users.loc[users["username"] == owner]["name", "surname"])
+
+
 if __name__ == "__main__":
-    df = pd.read_csv("database.csv")
-    print(df.iloc[1]["producer"])
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-from database import login, register, dashboard, add
+from database import login, register, dashboard, add, username_exist, show_cars, show_car
 
-
-enter = input("login or register? ")
+enter = ""
+user = 0
 
 while (enter != "exit"):
-    if enter == "login":
-        user = input("Enter username ")
-        password = input("Enter password ")
-        ans = login(user, password)
-        if (user != 0):
-            dashboard(user)
-    break
+    if user == 0:
+        enter = input("log in or register? ")
+        if enter == "log in":
+            user = input("Enter username ")
+            password = input("Enter password ")
+            ans = login(user, password)
+            if (ans == 0):
+                print("No account like this was found")
+                continue
+        if enter == "register":
+            user = input("Create a username: ")
+            if username_exist(user):
+                passwordofuser = input("Create a password: ")
+                while len(passwordofuser) < 8:
+                    passwordofuser = input(
+                        "It should be 8 characters minimum: ")
+                nameofuser = input("Enter name ")
+                surnameofuser = input("Enter surname ")
+                bdofuser = input("Enter birthday ")
+                register(user, nameofuser, surnameofuser,
+                         bdofuser, passwordofuser)
+    if user != "":
+        enter = input("""-Buy a car
+-Sell a car
+-My account
+-Log out
+""")
+    if enter == "buy a car":
+        show_cars()
+        enter = input("which car you wanna buy? ")
+        show_car(enter)
+    if enter == "sell a car":
+        producer = input("Enter the producer of the car: ")
+        model = input("Enter the model of the car: ")
+        type = input("Enter the type of the car (sedan, suv, sportcar,...): ")
+        year = input("Enter the year of production of the car: ")
+        hp = input("Enter the hp of the car: ")
+        cap = input("Enter the engine capacity of the car: ")
+        color = input("Enter the color of the car: ")
+        add(producer, model, type, year, hp, cap, color, user)
+    if enter == "my account":
+        dashboard(user)
+    if enter == "Log out":
+        user = 0
+
 
 """
 - create account:
